675a8630eac74458288e3df87bcb8f977deed082
ioilmio <illuminato.salvatore@gmail.com>
2020-09-23 00:26:13 +0200

    user_tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,21 @@ RSpec.describe User, type: :model do
     it { should have_many(:opinions) }
   end
   describe 'validations' do
+    # before do
+    it 'expect user to be valid' do
+      user = User.create!(
+        username: 'user',
+        fullname: 'user one',
+        email: 'user21@mail.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      # end
+      expect(user).to be_valid
+    end
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:fullname) }
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
-    # it { is_expected.to validate_uniqueness_of(:email) }
   end
 end
