a26204cd9fd9c6c890844f86405c56bc281ab48d
ioilmio <illuminato.salvatore@gmail.com>
2020-09-23 00:12:26 +0200

    user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,13 @@ class User < ApplicationRecord
 
   validates :fullname, presence: true, length: { maximum: 50 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/.freeze
 
-  validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, length: { maximum: 50 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+
+  validates :password, presence: true, length: { minimum: 8 }
+
+  validates :password_confirmation, presence: true, length: { minimum: 8 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe User, type: :model do
     it { should have_many(:opinions) }
   end
   describe 'validations' do
-    # it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:username) }
     it { should validate_presence_of(:fullname) }
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
-    # it { should validate_uniqueness_of(:email) }
+    # it { is_expected.to validate_uniqueness_of(:email) }
   end
 end
