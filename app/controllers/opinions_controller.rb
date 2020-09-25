113b6288215ad663e4b80d0a36da287d5e70f1da
ioilmio <illuminato.salvatore@gmail.com>
2020-09-25 23:01:24 +0200

    fix image alignment

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -24,7 +24,6 @@ class OpinionsController < ApplicationController
 
   def create
     @opinion = current_user.opinions.build(opinion_params)
-    # @opinion.image.attach(params[:opinion][:image])
     @opinion.image.attach(params[:opinion][:image]) if params[:opinion][:image].present?
     respond_to do |format|
       if @opinion.save
@@ -38,8 +37,10 @@ class OpinionsController < ApplicationController
   end
 
   def update
+    @opinion.image.attach(params[:opinion][:image]) if params[:opinion][:image].present?
     respond_to do |format|
       if @opinion.update(opinion_params)
+
         format.html { redirect_to @opinion, notice: 'Opinion was successfully updated.' }
         format.json { render :show, status: :ok, location: @opinion }
       else
