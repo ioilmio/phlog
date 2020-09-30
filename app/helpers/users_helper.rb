6912816ec6a2e68bb6e63ab6b3b5caae10d7462a
ioilmio <illuminato.salvatore@gmail.com>
2020-09-30 05:19:25 +0200

    fix style; partials for follow;

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,11 +4,10 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, class: 'gravatar')
   end
+  if Rails.env == 'development'
+    def gravatar_for(user, size:80)
+      gravatar_url = "https://api.adorable.io/avatars/80/#{user.email.downcase}.png"
+      image_tag(gravatar_url, alt: user.username, class: 'gravatar')
+    end
+  end
 end
-
-# def gravatar_for(user, size: 80)
-#   gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-#   # gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-#   gravatar_url = "https://api.adorable.io/avatars/80/#{user.email.downcase}.png"
-#   image_tag(gravatar_url, alt: user.username, class: 'gravatar')
-# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 User.create!(username: 'ioilmio',
-  fullname: 'ioilmio',
-  email: 'ioilmio@adorable.io', password: 'password',
-  password_confirmation: 'password')
+             fullname: 'ioilmio',
+             email: 'ioilmio@adorable.io', password: 'password',
+             password_confirmation: 'password')
 
 20.times do |n|
   name = Faker::Name.name
@@ -18,7 +18,7 @@ end
 users = User.order(:created_at).take(6)
 5.times do
   content = Faker::Lorem.sentence
-  users.each { |user| user.opinions.create!(content: content) }
+  users.each { |user| user.opinions.create!(content: content + user.username.to_s) }
 end
 
 users = User.all
