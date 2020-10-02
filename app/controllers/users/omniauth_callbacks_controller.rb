16bf5e31292fb5fe02f09eabae3271e56d43603a
ioilmio <illuminato.salvatore@gmail.com>
2020-10-03 01:22:06 +0200

    rm all

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,46 +1,46 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  skip_before_action :verify_authenticity_token, only: %i[github]
-  # def facebook
-  #   @user = User.from_omniauth(request.env['omniauth.auth'])
+# class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+#   skip_before_action :verify_authenticity_token, only: %i[github]
+#   # def facebook
+#   #   @user = User.from_omniauth(request.env['omniauth.auth'])
 
-  #   if @user.persisted?
-  #     sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-  #     set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-  #   else
-  #     session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra)
-  #     redirect_to new_user_registration_url
-  #   end
-  # end
+#   #   if @user.persisted?
+#   #     sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+#   #     set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+#   #   else
+#   #     session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra)
+#   #     redirect_to new_user_registration_url
+#   #   end
+#   # end
 
-  def github
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-    if @user.persisted?
-      sign_in_and_redirect root_path#, event: :authentication # this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format?
-    else
-      session['github_data'] = request.env['omniauth.auth'].except(:extra)
-      redirect_to new_user_registration_url
-    end
-  end
+#   def github
+#     @user = User.from_omniauth(request.env['omniauth.auth'])
+#     if @user.persisted?
+#       sign_in_and_redirect root_path#, event: :authentication # this will throw if @user is not activated
+#       set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format?
+#     else
+#       session['github_data'] = request.env['omniauth.auth'].except(:extra)
+#       redirect_to new_user_registration_url
+#     end
+#   end
 
-  def failure
-    redirect_to root_path
-  end
+#   def failure
+#     redirect_to root_path
+#   end
 
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
+#   # GET|POST /resource/auth/twitter
+#   # def passthru
+#   #   super
+#   # end
 
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
+#   # GET|POST /users/auth/twitter/callback
+#   # def failure
+#   #   super
+#   # end
 
-  # protected
+#   # protected
 
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
-end
+#   # The path used when OmniAuth fails
+#   # def after_omniauth_failure_path_for(scope)
+#   #   super(scope)
+#   # end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
-    omniauth_callbacks: 'users/omniauth_callbacks'
+    # omniauth_callbacks: 'users/omniauth_callbacks'
   }
   resources :users do
     member do
