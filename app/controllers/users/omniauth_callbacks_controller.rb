e76fad59711af1accbeffc81b57e7120941969b2
ioilmio <illuminato.salvatore@gmail.com>
2020-10-02 23:55:16 +0200

    devise session data

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -18,7 +18,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user#, event: :authentication # this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format?
     else
-      session['devise.github_data'] = request.env['omniauth.auth'].except(:extra)
+      session['github_data'] = request.env['omniauth.auth'].except(:extra)
       redirect_to new_user_registration_url
     end
   end
