e146ee1b984b0858a5a7665f4e0d4b1e42ec1fb9
ioilmio <illuminato.salvatore@gmail.com>
2020-10-03 01:27:42 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :opinions
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/sessions',
+    sessions: 'users/sessions'
     # omniauth_callbacks: 'users/omniauth_callbacks'
   }
   resources :users do
